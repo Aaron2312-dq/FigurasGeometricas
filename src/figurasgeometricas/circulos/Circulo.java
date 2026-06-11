98704c96896610d8d691cd3e771f95b01f03bbf6
Aaron2312-dq <aadiazqu@est.utn.ac.cr>
2026-06-11 13:36:15 -0600

    Se refactorizo la clase Circulo
    Se elimino la variable diamtro, en la parte de calcular se cambio de public a protected

--- src/figurasgeometricas/circulos/Circulo.java
+++ src/figurasgeometricas/circulos/Circulo.java
@@ -11,7 +11,6 @@ import figurasgeometricas.Figuras;
  */
 public class Circulo extends Figuras {
     private double radio;
-    private double diametro;
     private static final double PI = Math.PI;
 
     public double getRadio() {
@@ -19,7 +18,7 @@ public class Circulo extends Figuras {
     }
 
     public double getDiametro() {
-        return diametro;
+        return this.calcularDiametro();
     }
 
     public static double getPI() {
@@ -36,23 +35,20 @@ public class Circulo extends Figuras {
     }
     
     //Como calcular diametro
-    public double calcularDiametro(){
-        this.diametro  = radio*2;
-        return diametro;
+    protected double calcularDiametro(){
+        return radio * 2;
     }    
     
     //Cacula perimetro y area esto es heredado de la clase figura
     @Override
-    public double calcularPerimetro() {
-        this.perimetro = 2 * Circulo.PI*this.radio;
-        return perimetro;
+    protected double calcularPerimetro() {
+        return 2 * Circulo.PI*this.radio;
+        
     }
 
     @Override
-    public double calcularArea() {
-        this.area = Math.pow(radio, 2) * Circulo.PI;
-        return area;
-    }
-    
-    
+    protected double calcularArea() {
+        return Math.pow(radio, 2) * Circulo.PI;
+   
+    }  
 }
